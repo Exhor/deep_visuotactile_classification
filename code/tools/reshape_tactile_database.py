6e8511396d670e2371d127d5293dc61ebf460ebe
Tadeo Corradi <t.corradi@gmail.com>
2017-05-17 14:41:17 +0100

    added tactile db tool

---
+++ code/tools/reshape_tactile_database.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Reshape old style tactile database (filename structure) to (folder structure)
+"""
+import os
+if __name__ == '__main__':
+    loc = '/home/tadeo/data/c10/c10/c10_dry_touch'
+    d = os.listdir(loc)
+    
