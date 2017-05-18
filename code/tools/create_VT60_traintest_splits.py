a7dbf6ee6b521eb912838a55c3d31b864e47eb0c
Exhor <t.corradi@gmail.com>
2017-05-18 10:27:29 +0100

    renamed database, finished traintestsplit

--- code/tools/create_VT60_traintest_splits.py
+++ code/tools/create_VT60_traintest_splits.py
@@ -6,15 +6,28 @@ Purpose is classification
 
 @author: tadeo
 """
-import random
-if __name__ == '__main__':
+import random, os, numpy as np
+
+def train_test_split(path, seed, ntrain=40, ntest=40):
+    """ Returns (tr, te). Each tX[i = 0..ntrain/ntest][0=vis 1=tch][0=imgpath 1=imglabel] """
     random.seed(a=1) # repeatability
-    fname = 'train-test_splits.txt'
-    t = dict()  # t[ntrial][0][i][0] is the ith file for training
-                # t[ntrial][1][i][0] is the ith file for testing
-                # ...............[1] is the label
-    ntrials = 100
-    for trial in range(ntrials):
-        test_instance = random.randint(1,6)
-        
-        
+    test_instance = random.randint(1, 6)
+    te = []
+    tr = []
+    class_paths = sorted(os.listdir(path + 'vision/')) # same for touch
+    for c, cpath in enumerate(class_paths):
+        for modality, mpath in enumerate(['vision/','touch/']):
+            impaths = os.listdir(path + mpath + cpath + '/0' + str(test_instance))
+            paths = np.random.choice(impaths, ntest, replace=False)
+            te += [[impath, c+1] for impath in paths]
+            for train_instance in range(1,7):
+                if train_instance != test_instance:
+                    impaths = os.listdir(path + mpath + cpath + '/0' + str(train_instance))
+                    paths = np.random.choice(impaths, ntrain, replace=False)
+                    tr += [[impath, c+1] for impath in paths]    
+    return (tr, te)
+
+if __name__ == '__main__':
+    path = '/home/tadeo/p5/deep_visuotactile_classification/code/data/vt60/'
+    seed = 1
+    a = train_test_split(path, seed, ntrain=2, ntest=1)
