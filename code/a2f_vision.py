3c403f5c1d831b50df455c9a476f2741a00fa766
Exhor <t.corradi@gmail.com>
2017-07-19 07:57:31 +0100

    latefusion deeplearning for both modalities seems to work

--- code/a2f_vision.py
+++ code/a2f_vision.py
@@ -109,33 +109,40 @@ def m1_dense(inshape, optimiser='adadelta'):
     m.compile(optimizer=optimiser, loss=categorical_crossentropy, metrics=['accuracy'])
     return m
 
-def m2_deepfusion(vshape, tshape, optimiser='adadelta'):
-    x = Input(vshape)
-    x = Dense(256, activation='relu', input_shape=inshape)(x)
+def m2_latefusion(vshape, tshape, optimiser='adadelta'):
+    xin = Input((vshape[0]+tshape[0],))
+    x = Dense(256, activation='relu')(xin)
     x = Dropout(0.25)(x)
     x = Dense(32, activation='relu')(x)
     x = Dropout(0.25)(x)
+    xout = Dense(10, activation='softmax')(x)
+    m = Model(xin, xout)
+    m.compile(optimizer=optimiser, loss=categorical_crossentropy, metrics=['accuracy'])
+    return m
 
-def vt60_touch_data():
+def vt60_touch_data_aligned(v, ntouches=1):
+    ''' Load VT60 database touch data and align it with visual data <v> '''
     n_labels = 10
     n_instances = 6
-    n_imgs = 120
-    tdata = np.load('cache/vt60_touch_encoded_m4dcae')
+    n_train_t = 60
+    n_v_imgs = v.shape[2]
+    tdata = np.load('cache/vt60_touch_encoded_m4dcae.npy')
     d = tdata[()] # unpack dict
-    n_obj, n_img = d['xtrain'].shape[:2]
-    encoded_dims = d['xtrain'].shape[2:]
-    v = np.zeros((n_labels,
+    n_obj, n_t_imgs = d['xtrain'].shape[:2]
+    encoded_touch_dim = np.prod(d['xtrain'].shape[2:]) * ntouches
+    vt = np.zeros((n_labels,
                   n_instances,
-                  n_imgs,) +
-                 encoded_dims)
+                  n_v_imgs,
+                  25088 + encoded_touch_dim))
     for obj in range(n_obj):
         f = d['files_train'][obj]
         # '/home/tadeo/a2/code/data/vt60/touch/05_shoe/01/'
         s = [m.start() for m in re.finditer('/', f)]
         instance = int(f[s[-2]+1:s[-1]]) - 1
         label = int(f[s[-3]+1:s[-3]+3]) - 1
-        v[label, instance] = d['xtrain'][obj]
-    return v
+        tbunch = [d['xtrain'][obj][np.random.permutation(n_train_t)[:ntouches]].flatten() for i in range(n_v_imgs)]
+        vt[label, instance] = np.concatenate((v[label, instance], np.array(tbunch)), axis=1)
+    return vt
 
 
 from keras.callbacks import LambdaCallback
@@ -143,6 +150,8 @@ from keras.callbacks import LambdaCallback
 if __name__ == '__main__':
     blotch = False
     fusion = True
+    ntouches_allowed = [1, 3, 5, 10, 15, 20]
+
     # ktf.set_session(get_session()) # GPU vram use: grow as needed instead of hogging all at start
     # base = VGG16(weights='imagenet')
     # f_model = Model(inputs=base.input, outputs=base.get_layer('flatten').output)
@@ -150,40 +159,43 @@ if __name__ == '__main__':
     # v = vt60_vision_data(width = 224, encode=f, encoded_dims=(25088,), blotch=blotch)
     # np.save('cache/vision_vgg16_encoded' + '_blotched'*blotch, v)
     v = np.load('cache/vision_vgg16_encoded' + '_blotched'*blotch + '.npy')
-    t = vt60_touch_data()
     optimiser = 'adadelta'
-    epochs = 50
+    epochs = 20
     n_testmigs = 40
-
-    for n_trainimgs in [40, 30, 20, 15, 10, 8, 5, 3, 2, 1]:
-        print('Finetuning VGG16 using %s. Training with %d images.' % (optimiser, n_trainimgs))
-        n_test = 10 * n_testmigs # number of test samples
-        cm = np.zeros((60, 10))  # confusion matrix objid -> pred class
-        ypred = np.zeros((6, n_test), dtype='uint8')
-        ytrue = np.zeros((6, n_test), dtype='uint8')
-        vprob = np.zeros((6, n_test, 10)) # p(c|v)
-        for test_instance in range(6):
+    for ntouches in ntouches_allowed:
+        vt = vt60_touch_data_aligned(v, ntouches=ntouches)
+        for n_trainimgs in [40, 30, 20, 15, 10, 8, 5, 3, 2, 1]:
+            print('Finetuning VGG16 using %s. Training with %d images.' % (optimiser, n_trainimgs))
             if fusion:
-                xtr, ytr, xte, yte, xva, yva = split_by_instance(v, test_instance=test_instance,
-                                                                 n_imgs_train=n_trainimgs)
+                print('ntouches = ', ntouches)
+            n_test = 10 * n_testmigs # number of test samples
+            cm = np.zeros((60, 10))  # confusion matrix objid -> pred class
+            ypred = np.zeros((6, n_test), dtype='uint8')
+            ytrue = np.zeros((6, n_test), dtype='uint8')
+            vprob = np.zeros((6, n_test, 10)) # p(c|v)
+            for test_instance in range(6):
 
-            else:
-                xtr, ytr, xte, yte, xva, yva = split_by_instance(v, test_instance=test_instance, n_imgs_train=n_trainimgs)
+                    # train
+                    if fusion:
+                        m = m2_latefusion(vshape=(25088,), tshape=(36*ntouches,), optimiser=optimiser)
+                        x = vt
+                    else:
+                        x = v
+                        m = m1_dense((25088,), optimiser)
 
-                # train
-                m = m1_dense(xtr.shape[1:], optimiser)
-                cb = LambdaCallback(on_epoch_end= lambda epoch,logs: print('tr_acc = ', logs.get('acc'), ' # val_acc = ', logs.get('val_acc'))) # show progress
-                m.fit(xtr, ytr, batch_size=40, epochs=epochs, shuffle=True, verbose=0, validation_split=0.1, callbacks=[cb])
+                    xtr, ytr, xte, yte, xva, yva = split_by_instance(x, test_instance=test_instance,
+                                                                     n_imgs_train=n_trainimgs)
 
-                # test & record
-                vp = m.predict(xte)
-                vprob[test_instance] = vp
-                ypred[test_instance] = np.argmax(vp, axis=1)
-                ytrue[test_instance] = np.argmax(yte, axis=1)
-                print()
-                print('Test accuracy for instance %d: %.3f' % (test_instance, np.mean(ypred[test_instance]==ytrue[test_instance])))
-                for i in range(n_test):
-                    cm[ytrue[test_instance,i]*6 + test_instance, ypred[test_instance,i]] += 1
-                #np.save('cache/vision_vgg16_pred_true_instance_%d' % test_instance, (ypred, ytrue))
-        scipy.io.savemat('cache/vision_vgg16_256_32_%s_epochs_%d_ntrainv_%d_pred_true_cm_pred_vprob%s.mat' % (optimiser, epochs, n_trainimgs, '_blotched'*blotch), {'ypred':ypred, 'ytrue':ytrue, 'cm':cm, 'vprob':vprob})
-    #np.save('cache/vision_vgg16_cm', cm)
+                    cb = LambdaCallback(on_epoch_end= lambda epoch,logs: print('tr_acc = ', logs.get('acc'), ' # val_acc = ', logs.get('val_acc'))) # show progress
+                    m.fit(xtr, ytr, batch_size=40, epochs=epochs, shuffle=True, verbose=0, validation_split=0.1, callbacks=[cb])
+
+                    # test & record
+                    vp = m.predict(xte)
+                    vprob[test_instance] = vp
+                    ypred[test_instance] = np.argmax(vp, axis=1)
+                    ytrue[test_instance] = np.argmax(yte, axis=1)
+                    print()
+                    print('Test accuracy for instance %d: %.3f' % (test_instance, np.mean(ypred[test_instance]==ytrue[test_instance])))
+                    for i in range(n_test):
+                        cm[ytrue[test_instance,i]*6 + test_instance, ypred[test_instance,i]] += 1
+            scipy.io.savemat('cache/vt_fusion_vgg16_m4dcae_256_32_%s_epochs_%d_ntrainv_%d_ntouch_%d_%s.mat' % (optimiser, epochs, n_trainimgs,ntouches, '_blotched'*blotch), {'ypred':ypred, 'ytrue':ytrue, 'cm':cm, 'vprob':vprob})
